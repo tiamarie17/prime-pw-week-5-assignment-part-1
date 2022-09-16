7e73647d7ec7277414b3e2cc776b1b530c495338
Tia Theisen <tiatheisen@gmail.com>
2022-09-15 23:14:34 -0500

    accessed object properties using dot notation

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,8 +9,13 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
-  // TODO - add properties here
+  firstName: 'Tia',
+  lastName: 'Theisen',
+  hasSiblings: false,
+  shoeCount: 3,
+  favThreeFoods: ['pumpkin pie', 'chocolate ice cream', 'egg tarts'],
 };
+
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
@@ -20,12 +25,20 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
+let fullName= me.firstName + ' ' + me.lastName;
+console.log(fullName);
+
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
+
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[2]);
+
+
 
 
 
@@ -36,6 +49,11 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+console.log(me.shoeCount);
+me.shoeCount += 1;
+console.log(me.shoeCount);
+
+
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +61,8 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'green'
+console.log(me);
+
+
